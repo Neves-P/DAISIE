e3b6b8b453337d871da7d77a1345596aa9a26047
Neves-P <10762799+Neves-P@users.noreply.github.com>
2018-04-21 00:25:23 +0200

    Add ext rate documentation. Merge documentation. Correct Apars indices

--- R/DAISIE_aux.R
+++ R/DAISIE_aux.R
@@ -1,6 +1,6 @@
 # Function to describe changes in area through time. Adapted from
 # Valente et al 2014 ProcB
-island_area<-function(t, time, Apars,shape = NULL){
+island_area<-function(t, time, Apars, shape = NULL){
   if(is.null(shape)){return(Apars[1])}	
   if(shape == "quadratic"){
     if(Apars[2] > time){
@@ -28,20 +28,21 @@ island_area<-function(t, time, Apars,shape = NULL){
   }
 }
 
-
+##### DOUBLE CHECK APARS INDICES
 
 # Function to describe changes extinction rate through time. Adapted from
 # Valente et al 2014 ProcB
 getExtRate <- function(t, time, Apars, mu, shape, extcutoff, mu_version){
+  Epars <- c()
   Epars[1] <- max(0.01, mu - mu/2) # Epars contains mu_min and mu_max for quadratic and logistic
   Epars[2] <- mu + mu/2
   
   if(mu_version == "procb"){
     X <- log(Epars[1] / Epars[2]) / log(0.1)
-    extrate <- Epars[1] / ((island_area(t, time, Apars, shape) / Apars[1])^X) 
+    extrate <- Epars[1] / ((island_area(t, time, Apars, shape) / time)^X) 
   }else if(mu_version == "muMax_power"){
     X <- mu
-    extrate <- -((island_area(t, Apars, shape) / Apars[2]))^(X) + 1 + Epars[1] # Epars1 is basal extinction
+    extrate <- -((island_area(t, time, Apars, shape) / time))^(X) + 1 + Epars[1] # Epars1 is basal extinction
   }else if(mu_version == "logistic"){
     extrate <- Epars[1] + 1 / (island_area(t, time, Apars, shape) + 10) # logistic function
   }else{
@@ -53,4 +54,5 @@ getExtRate <- function(t, time, Apars, mu, shape, extcutoff, mu_version){
   # print(island_area(t, Apars, shape))
   return(extrate)
 }
+
 
